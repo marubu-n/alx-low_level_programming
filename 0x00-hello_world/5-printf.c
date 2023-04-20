139b8d310defc50ab762984d838e7be40d075408
marubu-n <rodahnjoki70@gmail.com>
2023-04-20 01:41:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: '5-printf.c'
+*
+*Return: Always 0 (Succuss)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
