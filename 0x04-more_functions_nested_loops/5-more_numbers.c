98dd93913c5c53aa44d96f8901a559fb6c8db754
marubu-n <rodahnjoki70@gmail.com>
2023-04-28 07:27:34 -0700

    5. Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,27 @@
 #include "main.h"
-/**
-*more_numbers - print more numbers
-*Retuen: 0
-*/
 
+/**
+ * more_numbers - function that checks for uppercase character
+ * Return: 0
+ */
 void more_numbers(void)
 {
-	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	int a = 0;
+	int b = 0;
+
+	while (b <= 9)
 	{
-		for (j = 0; j <= 14; j++)
+		while (a <= 14)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			a++;
+
 		}
-		_putchar('\n');
+	_putchar('\n');
+	b++;
+	a = 0;
 	}
 }
