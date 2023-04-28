a04ce39c4cbf6d29a6125169b5de8652d0837327
marubu-n <rodahnjoki70@gmail.com>
2023-04-28 07:33:57 -0700

    6. The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,23 @@
 #include "main.h"
-/**
-*print_line - draws a straight line in the terminal
-*@n: number f times the character _ should be printed
-*/
 
+/**
+ * print_line - function that checks for uppercase character.
+ * @n: is the integer for the paramaters of my function
+ * Return: 0
+ */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
+	int a = 0;
 
-		for (i = 1; i <= n; i++)
+	if (n > 0)
+	{
+		while (a <= n)
 		{
 			_putchar('_');
+			a++;
 		}
-		_putchar('\n');
+	 _putchar('\n');
 	}
+	else
+	_putchar('\n');
 }
