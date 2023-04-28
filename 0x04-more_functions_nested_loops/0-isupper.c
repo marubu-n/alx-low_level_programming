d4d90a608b5c931d6d112230652e15a566aab912
marubu-n <rodahnjoki70@gmail.com>
2023-04-28 05:43:31 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,7 +10,7 @@
 */
 int _isupper(int x)
 {
-	if (x >= 65 && x <= 90)
+	if (x >= 'A' && x <= 'Z')
 	{
 		return (1);
 	}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*mul - multiplt 2 int
+*@a: first int
+*@b: second int
+*Reurn: addition of the multiplication
+*/
+
+int mul(int a, int b)
+{
+	return(a*b);
+}
